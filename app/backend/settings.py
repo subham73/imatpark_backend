616ebcc28ad2f01f23d5c77bc086cb495e282de2
Subham Jyoti <subhamjyoti1@gmail.com>
2024-04-05 09:56:31 +0530

    formated .flake8 file

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = SECRET_KEY = 'django-insecure-ulki60x(t^o=jp7j$rdtq$q^a!uei0h6h^n_y$o$zt^30y8hf-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", default=0))
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
