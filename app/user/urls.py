40e836a659e4a3816f7ca8ddf6851f5608e9219e
Subham Jyoti <subhamjyoti1@gmail.com>
2024-10-26 02:20:41 +0530

    sample user analytics code&test

--- app/user/urls.py
+++ app/user/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
     path('token/', views.CreateTokenView.as_view(), name='token'),
     path('me/', views.ManageUserView.as_view(), name='me'),
-    # path('analytics/', views.UserLogAnalyticsView.as_view(),
+    path('analytics/', views.UserLogAnalyticsView.as_view(), name='analytics'),
     #  name='user-log-analytics'),
 ]
